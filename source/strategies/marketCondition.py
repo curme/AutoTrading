acf11c7335e6bc2fa5dd498ff0c261245c30ad50
juwannaman <shuihanyi@Shuihans-MacBook-Pro.local>
2016-05-01 14:19:42 +0800

    rename filter.py to marketCondition.py

--- source/strategies/marketCondition.py
+++ source/strategies/marketCondition.py
@@ -4,7 +4,7 @@ Filter for strategies selection based on (x1, x2, x3 ...)
 
 def filter(data):
     """
-    Call different strategies based on different market condition
+    distinguish different strategies based on different market condition
     :param data:
     :return:
     """
