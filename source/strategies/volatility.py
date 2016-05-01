671966102314509b04bb21526df5092a0df03221
Brandon Yan <freexyan@gmail.com>
2016-05-02 00:45:33 +0800

    volatility.py

--- source/strategies/volatility.py
+++ source/strategies/volatility.py
@@ -1,5 +1,4 @@
 import pandas as pd
-<<<<<<< HEAD
 import talib as tl
 import numpy as np
 from talib import MA_Type
@@ -9,20 +8,11 @@ from source.data.preprocessing import Data
 
 #dataPre
 
-=======
-import numpy as np
-from source.data.preprocessing import Data
-from source.data.indicators import volatility
-
-#dataPre
-
->>>>>>> master
 if __name__ == "__main__":
 
     dt = Data()
     vl = volatility()
     df = dt.getExcelInterval(pd.Timestamp("2015-10-26 16:00:00"),pd.Timestamp("2016-03-26 16:00:00"))
-<<<<<<< HEAD
 
     ave,upband,dnband = vl.bollingerBand(df['Close'],30)
 
@@ -47,16 +37,5 @@ if __name__ == "__main__":
             signal = ["HSI",df.loc[i,'Date'],df.loc[i,'Close'],"Buy"]
         signals.append(signal)
     signals = pd.DataFrame(signals)
-=======
-    ave,upband,dnband = vl.bollingerBand(df['Close'],30)
-    signals = pd.DataFrame()
-    print df.loc[30,'Close']
-    for i in range(29,len(ave)):
-        if df.loc[i,'Close'] > upband[i]:
-            signal = ["HSI",df.loc[i,'Date'],df.loc[i,'Close'],"Sell"]
-        if df.ix[i,'Close'] < dnband[i]:
-            signal = ["HSI",df.loc[i,'Date'],df.loc[i,'Close'],"Buy"]
-        signals.append(signal)
->>>>>>> master
     print signals
 
