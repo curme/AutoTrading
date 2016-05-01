b884aae1fee4ddd0751c3e2ffa870083347242b0
Angela Zhou <zaq931228@gmail.com>
2016-05-01 15:07:44 +0800

    bollingerBandCalculation
    bollingerBand

--- source/strategies/indicators.py
+++ source/strategies/indicators.py
@@ -11,6 +11,7 @@ Volume      |Chaikin Oscillator, OBV, Rate of Change(ROV)
 """
 
 import pandas as pd
+import numpy as np
 
 """ momentum indicators """
 class momentum:
@@ -28,6 +29,11 @@ class volatility:
     def __init__(self):
         pass
 
-    def bollingerBand(self):
-        pass
+    def bollingerBand(self,price, length=30, numsd=2):
+        """ returns average, upper band, and lower band"""
+        ave = pd.stats.moments.rolling_mean(price,length)
+        sd = pd.stats.moments.rolling_std(price,length)
+        upband = ave + (sd*numsd)
+        dnband = ave - (sd*numsd)
+        return np.round(ave,3), np.round(upband,3), np.round(dnband,3)
 
