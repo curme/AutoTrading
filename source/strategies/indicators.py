18068d1745257210d499c4c3198b3646544490cb
juwannaman <shuihanyi@Shuihans-MacBook-Pro.local>
2016-05-01 14:21:10 +0800

    create indicator.py
    indicator.py define indicator functions

---
+++ source/strategies/indicators.py
@@ -0,0 +1,33 @@
+"""
+Indicators will be used to trigger strategies
+---------------------------------------------------------
+Type        |Examples
+---------------------------------------------------------
+Trend       |Moving Average, MACD, Parabolic SAR
+Momentum    |Stochastic, CCI, RSI
+Volatility  |Bollinger Band, Average True Range, Std
+Volume      |Chaikin Oscillator, OBV, Rate of Change(ROV)
+---------------------------------------------------------
+"""
+
+import pandas as pd
+
+""" momentum indicators """
+class momentum:
+    def __init__(self):
+        pass
+
+    def movingAverage(self):
+        pass
+
+    def MACD(self):
+        pass
+
+""" volatility indicators """
+class volatility:
+    def __init__(self):
+        pass
+
+    def bollingerBand(self):
+        pass
+
