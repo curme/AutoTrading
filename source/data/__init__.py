ff965c5d1ed66d17155aab0cc15fc2d1e48ebad2
Brandon Yan <freexyan@gmail.com>
2016-05-01 15:13:52 +0800

    initiation && hsi_futures_ExcelFile
    data restored in df DataFrame
    access by:
    df.ix[0, "Date"]  (index , name)

---
+++ source/data/__init__.py
@@ -0,0 +1,17 @@
+from openpyxl import load_workbook
+import numpy as np
+import pandas as pd
+import xlrd
+import glob, os
+
+
+df = pd.DataFrame()
+xl = pd.ExcelFile("hsi_futures.xlsx")
+# Date | Open | High | Low | Close | SMAVG5 | SMAVG10 | SMAVG15 | Volume | VolumeSMAVG5
+print xl.sheet_names
+sheets = xl.sheet_names
+for sheet in sheets:
+    df = df.append(pd.read_excel("hsi_futures.xlsx", sheet))
+print df.ix[0, "Date"]
+
+#print df.ix['2/26/2016  3:30:00']
